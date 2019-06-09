06448ff5b211ec902538415a915e9ab72e3ff08f
Carsten Damm <carsten.damm@cgi.com>
2019-06-09 17:16:29 +0200

    .

--- unit_test.sh
+++ unit_test.sh
@@ -4,13 +4,11 @@
 cd resource-app-features
 app_output="$(./hello_world_app.sh)"
 
-echo $app_output
 
-ls
 
 if [ "$app_output" = "Hello World!" ]
 then
-    echo "unit test passed!"
+    echo "Output equals $app_output. Unit test passed!"
 else 
     echo "unit test failed!"
 fi
