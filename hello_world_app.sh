b69f4ea4d5ff0dc9ba92ccecc350ccac60b4cc02
Carsten Damm <carsten.damm@cgi.com>
2019-06-09 15:52:29 +0200

    .

--- hello_world_app.sh
+++ hello_world_app.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-mkdir hello_dir
-hello="Hello World!!"
-echo $hello > hello_dir/hello_file
+
+echo "Hello World!"
 
--- unit_test.sh
+++ unit_test.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
-echo "unit test passed!"
+app_output=./resource-app-features/hello_world_app.sh
+
+if [$app_output == "Hello World!"]
+then
+    echo "unit test passed!"
+else 
+    echo "unit test failed! Job paused."
+    fly pause-job -j hello-world/job-hello-world
 
