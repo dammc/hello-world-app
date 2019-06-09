a71d3f163e85c30facb289dc7d8b7487e665319c
Carsten Damm <carsten.damm@cgi.com>
2019-06-09 16:35:34 +0200

    .

--- unit_test.sh
+++ unit_test.sh
@@ -1,12 +1,11 @@
 #!/bin/sh
 
-app_output=./resource-app-features/hello_world_app.sh
+app_output=./hello_world_app.sh
 
 if [$app_output == "Hello World!"]
 then
     echo "unit test passed!"
 else 
-    echo "unit test failed! Job paused."
-    fly pause-job -j hello-world/job-hello-world
+    echo "unit test failed!"
 fi
 
